049c20fbdfbd94024f3171d4fc37585db881e93f
abuhamsa <haemsu@gmail.com>
2025-02-09 21:05:04 +0100

    testing wsl git

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import mkcert from 'vite-plugin-mkcert';
 
-// https://vite.dev/config/
+// https://vite.dev/config/ 
 export default defineConfig({
   plugins: [react(),mkcert()],
   server: {
